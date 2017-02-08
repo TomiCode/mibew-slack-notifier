131253555de508eea2ea90aed3d6261088de1407
Tomek K <tomicode@gmail.com>
2017-02-08 11:55:33 +0100

    Initial plugin commit.

---
+++ Plugin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Mibew\Mibew\Plugin\SlackNotifier;
+
+use Mibew\EventDispatcher\EventDispatcher;
+use Mibew\EventDispatcher\Events;
+use Mibew\Thread;
+
+class Plugin extends \Mibew\Plugin\AbstractPlugin implements \Mibew\Plugin\PluginInterface
+{
+  protected $config;
+
+  protected $initialized = false;
+
+  public function __construct($config)
+  {
+    if (empty($config['webhook_url'])) {
+      trigger_error('Slack Webhook URL cannot be empty', E_USER_ERROR);
+      return;
+    }
+
+   $this->initialized = true;
+   $this->config = $config + array(
+     'channel' => NULL,
+     'username' => NULL,
+     'message' => "A new visitor is waiting for an answer."
+   );
+  }
+
+  public function initialized()
+  {
+    return $this->initialized;
+  }
+
+  public function run()
+  {
+    $dispatcher = EventDispatcher::getInstance();
+    $dispatcher->attachListener(Events::THREAD_USER_IS_READY, $this, 'threadUserIsReady');
+  }
+
+  public function threadUserIsReady(&$args)
+  {
+    $thread = $args['thread'];
+    if ($thread->userId /* && invitationState == INVITATION_NOT_INVITED */ ) {
+      $this->sendSlackMessage();
+    }
+  }
+
+  private function sendSlackMessage()
+  {
+    $data_fields = [ 'text' => $config['message'] ];
+    if ($channel = $config['channel'])
+      $data_fields['channel'] = $channel;
+
+    if ($username = $config['username'])
+      $data_fields['username'] = $username;
+
+    $ch = curl_init($config['webhook_url']);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_fields);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+  }
+
+  public static function getVersion()
+  {
+    return '1.0.0';
+  }
+
+  public static function getSystemRequirements()
+  {
+    return array( 'mibew' => '^2.1.0' );
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var eventStream = require('event-stream'),
+  gulp = require('gulp'),
+  chmod = require('gulp-chmod'),
+  zip = require('gulp-zip'),
+  tar = require('gulp-tar'),
+  gzip = require('gulp-gzip'),
+  rename = require('gulp-rename');
+
+gulp.task('prepare-release', function() {
+  var version = require('./package.json').version;
+
+  return eventStream.merge(
+    getSources()
+      .pipe(zip('mibdew-slack-notifier-' + version + '.zip')),
+    getSources
+      .pipe(tar('mibdew-slack-notifier-' + version + '.tar'))
+      .pipe(gzip())
+  )
+  .pipe(chmod(0644))
+  .pipe(gulp.dest('release'));
+});
+
+gulp.task('default', ['prepare-release'], function() {});
+
+var getSources = function() {
+  return gulp.src([
+    'Plugin.php',
+    'README.md',
+    'LICENSE'
+  ],
+    {base: './'}
+  )
+  .pipe(rename(function(path) {
+    path.dirname = 'Mibew/Mibew/Plugin/SlackNotifier' + path.dirname;
+  }));
+};
